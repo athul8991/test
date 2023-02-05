8abe943db16dc2973965d5ac23cd8cab136eb6d7
Your Name <you@example.com>
2023-02-05 19:26:33 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const hostName="127.0.0.1";
+const port = 3000;
+const server = http.createServer((req,res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html') 
+    res.write("Hello world");
+    res.end()
+}).listen(port,hostName, ()=>{
+    console.log("Server Started at port : "+port);
+
+})
